ded0e530640fb6fa1acacc5272d4cd749eecb946
KrAnna7 <anna.dzioch@gmail.com>
2017-04-11 20:35:45 +0200

    zweryfikowanie zmian do choinki symetrycznej

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@ var n = 15;
 function drawTree(k) {
 	for (var i = 0 ; i < k ; i++) {
 		var star = '';
-		for (var j = 0 ; j < i ; j++) {
+		for (var j = 0; j < k - i ; j++) {
+			star += ' ';
+		}
+		for (var j = 0 ; j <= i * 2 ; j++) {
 			star += '*';
 		}
 		console.log(star);
